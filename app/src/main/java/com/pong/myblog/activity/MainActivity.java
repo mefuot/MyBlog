0d91f26cc982a37a81b557e06da54eaea6d149cd
Pong <pong230@gmail.com>
2017-01-10 12:59:26 +0700

    Change file location from default

--- app/src/main/java/com/pong/myblog/activity/MainActivity.java
+++ app/src/main/java/com/pong/myblog/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pong.myblog;
+package com.pong.myblog.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.pong.myblog.R;
 
 public class MainActivity extends AppCompatActivity {
 
