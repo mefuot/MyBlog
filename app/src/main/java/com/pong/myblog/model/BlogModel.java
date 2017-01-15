29a8df2ee12c033986ef434ff327e40597311ddf
Pong <pong230@gmail.com>
2017-01-15 15:39:12 +0700

    Update method and parametor for BlogModel. Prepare to implement with Database

--- app/src/main/java/com/pong/myblog/model/BlogModel.java
+++ app/src/main/java/com/pong/myblog/model/BlogModel.java
@@ -1,10 +1,24 @@
 package com.pong.myblog.model;
 
+import android.provider.BaseColumns;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by USER on 10/1/2560.
  */
 
 public class BlogModel {
+
+    public static class BlogColumns implements BaseColumns {
+        public static final String TABLE_NAME = "blog";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_CONTENT = "content";
+    }
+
+    int id;
     String title;
     String date;
     String content;
@@ -12,10 +26,19 @@ public class BlogModel {
     public BlogModel() {
     }
 
-    public BlogModel(String title, String date, String content) {
+    public BlogModel(int id,String title, String date, String content) {
+        this.id = id;
         this.title = title;
         this.date = date;
         this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -40,5 +63,18 @@ public class BlogModel {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public JSONObject toJson(){
+        try {
+            JSONObject obj = new JSONObject();
+            obj.put("title",title);
+            obj.put("date",date);
+            obj.put("content",content);
+            return obj;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
