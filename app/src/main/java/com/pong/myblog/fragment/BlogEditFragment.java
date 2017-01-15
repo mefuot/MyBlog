560c4221fbee1836d63e74e1b7687e16b10b1220
Pong <pong230@gmail.com>
2017-01-15 14:55:18 +0700

    Implement add content feature (rich text editor, add link dialog, date picker ) But add image is not include.

--- app/src/main/java/com/pong/myblog/fragment/BlogEditFragment.java
+++ app/src/main/java/com/pong/myblog/fragment/BlogEditFragment.java
@@ -1,19 +1,35 @@
 package com.pong.myblog.fragment;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.pong.myblog.R;
+import com.pong.myblog.model.BlogModel;
+
+import java.util.Calendar;
+
+import jp.wasabeef.richeditor.RichEditor;
 
 /**
  * Created by USER on 10/1/2560.
  */
 
 public class BlogEditFragment extends Fragment {
+    private RichEditor mEditor;
+    private EditText mTitle;
+    private EditText mDate;
 
     @Nullable
     @Override
@@ -24,5 +40,217 @@ public class BlogEditFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mTitle = (EditText) view.findViewById(R.id.edittext_title);
+        initDateFunction(view);
+        initContentEditor(view);
+
+        view.findViewById(R.id.button_add_new_blog).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                onAddNewBlog();
+            }
+        });
+    }
+
+    private void initDateFunction(@NonNull View view){
+        mDate = (EditText) view.findViewById(R.id.edittext_date);
+        final Calendar calendar = Calendar.getInstance();
+        mDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePicker = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, month);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        mDate.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                    }
+                },calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+                datePicker.show();
+            }
+        });
+    }
+
+    private void initContentEditor(@NonNull View view){
+        mEditor = (RichEditor) view.findViewById(R.id.editor);
+        mEditor.setEditorHeight(200);
+        mEditor.setEditorFontSize(18);
+        mEditor.setEditorFontColor(Color.BLACK);
+        mEditor.setPadding(10,10,10,10);
+
+        mEditor.setPlaceholder("Insert text here...");
+
+        view.findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setBold();
+            }
+        });
+
+        view.findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setItalic();
+            }
+        });
+
+        view.findViewById(R.id.action_subscript).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setSubscript();
+            }
+        });
+
+        view.findViewById(R.id.action_superscript).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setSuperscript();
+            }
+        });
+
+        view.findViewById(R.id.action_strikethrough).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setStrikeThrough();
+            }
+        });
+
+        view.findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setUnderline();
+            }
+        });
+
+        view.findViewById(R.id.action_heading1).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(1);
+            }
+        });
+
+        view.findViewById(R.id.action_heading2).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(2);
+            }
+        });
+
+        view.findViewById(R.id.action_heading3).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(3);
+            }
+        });
+
+        view.findViewById(R.id.action_heading4).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(4);
+            }
+        });
+
+        view.findViewById(R.id.action_heading5).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(5);
+            }
+        });
+
+        view.findViewById(R.id.action_heading6).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(6);
+            }
+        });
+
+        view.findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setIndent();
+            }
+        });
+
+        view.findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setOutdent();
+            }
+        });
+
+        view.findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setAlignLeft();
+            }
+        });
+
+        view.findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setAlignCenter();
+            }
+        });
+
+        view.findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setAlignRight();
+            }
+        });
+
+        view.findViewById(R.id.action_blockquote).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setBlockquote();
+            }
+        });
+
+        view.findViewById(R.id.action_insert_bullets).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setBullets();
+            }
+        });
+
+        view.findViewById(R.id.action_insert_numbers).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setNumbers();
+            }
+        });
+
+        view.findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.insertImage("http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
+                        "dachshund");
+            }
+        });
+
+        view.findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                showInputLinkDialog();
+            }
+        });
+    }
+
+    private void showInputLinkDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add link");
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View enterLinkView = inflater.inflate(R.layout.dialog_add_link,null);
+        final EditText mUrl = (EditText) enterLinkView.findViewById(R.id.edittext_link_url);
+        final EditText mLabel = (EditText) enterLinkView.findViewById(R.id.edittext_link_label);
+        builder.setView(enterLinkView);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if(mUrl.getText().toString().isEmpty()) return;
+                if(mLabel.getText().toString().isEmpty()){
+                    mLabel.setText(mUrl.getText().toString());
+                }
+                mEditor.insertLink(mUrl.getText().toString(), mLabel.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void onAddNewBlog(){
+        BlogModel blog = new BlogModel(mTitle.getText().toString(),
+                                        mDate.getText().toString(),
+                                        mEditor.getHtml());
+        Log.d("MyBlog",blog.getContent());
     }
 }
