2792894631272ebd8e00c5f4c7eae631ee212e28
Pong <pong230@gmail.com>
2017-01-23 16:06:36 +0700

    Setup toolbar for Blog preview page (and change other page too)

--- app/src/main/java/com/pong/myblog/activity/BlogPreviewActivity.java
+++ app/src/main/java/com/pong/myblog/activity/BlogPreviewActivity.java
@@ -3,7 +3,12 @@ package com.pong.myblog.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.pong.myblog.R;
 import com.pong.myblog.fragment.BlogPreviewFragment;
@@ -19,8 +24,28 @@ public class BlogPreviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
 
+        setupToolbar();
+
         if(savedInstanceState == null){
             showBlogPreviewFragment();
+        }
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setDisplayShowTitleEnabled(false);
         }
     }
 
@@ -32,5 +57,22 @@ public class BlogPreviewActivity extends AppCompatActivity {
         fragment.setArguments(bundle);
         ft.add(R.id.container, fragment);
         ft.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_blog_preview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_blogpreview_edit) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
